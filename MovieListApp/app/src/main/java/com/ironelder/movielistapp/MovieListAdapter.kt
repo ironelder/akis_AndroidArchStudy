d32f88503cf7b86fe7aa54ece0a10e448bc86522
jhjang <jhjang@10.11.52.125>
2020-12-22 14:52:35 +0900

    recyclerview adapter 상속

--- MovieListApp/app/src/main/java/com/ironelder/movielistapp/MovieListAdapter.kt
+++ MovieListApp/app/src/main/java/com/ironelder/movielistapp/MovieListAdapter.kt
@@ -1,10 +1,24 @@
 package com.ironelder.movielistapp
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class MovieListAdapter {
+class MovieListAdapter : RecyclerView.Adapter<MovieListAdapter.MovieListItem>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieListItem {
+//        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+//        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MovieListItem, position: Int) {
+//        TODO("Not yet implemented")
+    }
+
     class MovieListItem(view: View) : RecyclerView.ViewHolder(view){
-        
+
     }
 }
