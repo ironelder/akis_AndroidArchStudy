0a4a4ced6590601603a74ed0789bc89d05c1bacd
jhjang <jhjang@10.11.52.125>
2020-12-22 14:51:09 +0900

    뷰홀더 클래스 추가

--- MovieListApp/app/src/main/java/com/ironelder/movielistapp/MovieListAdapter.kt
+++ MovieListApp/app/src/main/java/com/ironelder/movielistapp/MovieListAdapter.kt
@@ -1,4 +1,10 @@
 package com.ironelder.movielistapp
 
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
 class MovieListAdapter {
+    class MovieListItem(view: View) : RecyclerView.ViewHolder(view){
+        
+    }
 }
